cc47933e390a4ac3a0bcadd2aa600c29b35b2ecf
Hoang Vu Dinh <dinh@SPSEJECNANET.CZ>
2019-01-08 08:39:14 +0100

    add wind form

--- Revision/Revision/Program.cs
+++ Revision/Revision/Program.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Revision
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Complex a = new Complex(4,8);
-            Complex b = new Complex(3, 9);
-            Complex c = b + a;
+            Application.EnableVisualStyles();
+            Application.Run(new TestForm());
+
+            //Complex a = new Complex(4, 8);
+            //Complex b = new Complex(3, 9);
+            //Complex c = b + a;
         }
     }
 }
