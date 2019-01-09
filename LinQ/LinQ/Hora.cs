0e1a611ab00cd38d5bfa201720fddce81b7e5e06
Pavel Dinh <pavel.dinh1@gmail.com>
2019-01-09 22:27:15 +0100

    LinQ

---
+++ LinQ/LinQ/Hora.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ
+{
+    class Hora
+    {
+        string jmeno;
+        int vyska;
+
+        public int Vyska { get { return vyska; }}
+        public string Jmeno { get; set; }
+
+        public Hora(string jmeno, int vyska)
+        {
+            this.jmeno = jmeno;
+            this.vyska = vyska;
+        }
+
+        public override string ToString()
+        {
+            return "Hora:" + jmeno + " Vyska:" + vyska;
+        }
+    }
+
+    class Pohori 
+    {
+        string jmeno_pohori;
+        List<Hora> hory;
+
+        public List<Hora> Hory { get; }
+        int pocetHor = 1;
+
+        public Pohori(string jmeno_pohori)
+        {
+            this.jmeno_pohori = jmeno_pohori;
+            hory = new List<Hora>();
+        }
+
+        public void PridejHoru(string jmeno, int vyska)
+        {
+            hory.Add(new Hora(jmeno, vyska));
+        }
+
+        public int PocetPohori()
+        {
+            return hory.Count;
+        }
+
+        public void VypisSerazeneHory()
+        {
+            var sortMount = from h in hory
+                            orderby h.Vyska descending
+                            select h;
+
+            foreach (Hora h in sortMount)
+            {
+                Console.WriteLine(h.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        public void VypisHoryNad(int par)
+        {
+            var sortMount = from h in hory
+                            where h.Vyska > par
+                            select h;
+            foreach(Hora h in sortMount)
+            {
+                Console.WriteLine(h.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        public void VypisHory()
+        {
+            foreach (Hora h in hory)
+            {
+                Console.WriteLine(h.ToString() + " " + pocetHor);
+                pocetHor++;
+            }
+            Console.WriteLine();
+        }
+
+        public override string ToString()
+        {
+            return "Pohori: " + jmeno_pohori + "\n";
+        }
+    }
+}
---
+++ LinQ/LinQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pohori p = new Pohori("Alpy");
+
+            p.PridejHoru("Matterhorn", 4478);
+            p.PridejHoru("dawd", 2465);
+            p.PridejHoru("hhfawdad", 3217);
+            p.PridejHoru("sda", 5456);
+            p.PridejHoru("asdttwe", 212);
+
+            p.VypisSerazeneHory();
+            p.VypisHoryNad(1000);
+            Console.WriteLine("Pocet Hor: " + p.PocetPohori());
+            Console.WriteLine(p.ToString());
+            Console.ReadKey();
+        }
+    }
+}
